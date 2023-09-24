69ba48a628fbeb8d1af72f4f508935451b2aa9af
CalyWorld <obinnaanizoba5@gmail.com>
2023-09-25 01:32:12 +0800

    finishing touches

--- app.js
+++ app.js
@@ -7,8 +7,25 @@ const logger = require("morgan");
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 const catalogRouter = require("./routes/catalog");
+const compression = require("compression");
 
 const app = express();
+app.use(compression());
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+    },
+  }),
+);
+// Set up rate limiter: maximum of twenty requests per minute
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+// Apply rate limiter to all requests
+app.use(limiter);
 require("dotenv").config();
 
 const mongoose = require("mongoose");
